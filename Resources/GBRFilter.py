106f73be8dc307a1afd1349da627f897914b3464
Dinith <tharusha.dinithma@gmail.com>
2022-09-19 14:26:15 +0530

    commit 11

--- Resources/GBRFilter.py
+++ Resources/GBRFilter.py
@@ -18,12 +18,21 @@ alldata = f.read()
 # Splits All the Data bt * and stores in an array
 arrayall = alldata.split('*')
 
+NewlineArray = []
+NewlineArray = arrayall[-2].split('\n')
+del NewlineArray[0]
+del NewlineArray[0]
+del NewlineArray[0]
 
 linearray=[]
 allarray=[]
 Nvalue=""
 Ndetected=False
 
+lastItem = ""
+LItem =False
+ArrayPostions=[]
+stringmake=""
 
 # Get the Length and the Width of the Marker
 def getGBRdata():
@@ -45,6 +54,15 @@ def getGBRdata():
     arrayReturn.append(round(float(MarkerWidth)*25.4, 0))
     return arrayReturn
 
+for items in NewlineArray:
+    if items.startswith('L'):
+        LItem = True
+        stringmake =stringmake[1:]
+        ArrayPostions.append(stringmake);
+        # print(stringmake)
+        stringmake=""
+    if LItem:
+        stringmake=stringmake+"/"+items
 
 for elements in arrayall:
     stripstring = elements.strip(' \n\t')
@@ -85,13 +103,15 @@ count = 0
 for var in INTALLarray:
     points=np.array(var)
     pts=np.array([points])
-    point = str(count)
+    point = ArrayPostions[count]
+    print(count, "  -  ", ArrayPostions[count+1])
+    pointC = str(count)
     img1 = cv2.line(img,(0,0),(511,511),(255,0,0),5)
     M = cv2.moments(pts)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
-    cv2.drawContours(img, pts, -1, (255, 225, 255), 13)
-    cv2.putText(img, point, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 9)
+    cv2.drawContours(img, pts, -1, (0, 225, 0), 13)
+    cv2.putText(img, pointC, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 9)
     count += 1
 
 half1 = cv2.resize(img1, (0, 0), fx=0.1, fy=0.1)
@@ -100,9 +120,16 @@ cv2.imshow("Stacked Images", half1)
 cv2.waitKey(0)
 
 value = input("Please enter a Shape:\n")
-value = int(value)
 
-points = np.array(INTALLarray[value])
+i = 0
+j = 0
+for x in ArrayPostions:
+    if (value in x):
+        j = i - 1
+        print(j)
+    i += 1
+
+points = np.array(INTALLarray[j])
 pts = np.array([points])
 cv2.drawContours(imgc, pts, -1, (0, 225, 0), 10)
 halfc = cv2.resize(imgc, (0, 0), fx = 0.1, fy = 0.1)
--- Resources/ReadLabel.py
+++ Resources/ReadLabel.py
@@ -1,7 +1,7 @@
 f = open("../GBR/one.GBR", "r", encoding='unicode_escape')  # reads the GBR file from the directory
 alldata = f.read()  # assign to all data variable
 # print(alldata)
-arrayall = alldata.split('*')  # Splits the All Data bt * and stores in a array
+arrayall = alldata.split('*')  # Splits All the Data bt * and stores in an array
 # print(arrayall[-2])
 NewlineArray = []
 NewlineArray = arrayall[-2].split('\n')
@@ -9,26 +9,29 @@ del NewlineArray[0]
 del NewlineArray[0]
 del NewlineArray[0]
 
-print(NewlineArray)
+# print(NewlineArray)
 lastItem = ""
 LItem =False
 ArrayPostions=[]
 stringmake=""
-for items in NewlineArray:
-    # print("Item")
 
+for items in NewlineArray:
     if items.startswith('L'):
         LItem = True
-
         stringmake =stringmake[1:]
         ArrayPostions.append(stringmake);
-        print(stringmake)
+        # print(stringmake)
         stringmake=""
-        # print(items)
-
     if LItem:
-        #print("Started L Detecetd")
         stringmake=stringmake+"/"+items
 
 
-print(ArrayPostions)
+word = '171/A,0,0,1,1/P,0,1,0,0'
+i = 0
+for x in ArrayPostions:
+    if (word in x):
+        # print(x)
+        print(i)
+    i += 1
+
+
